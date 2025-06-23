ec75da686f6fa1ba4c46899340f100926dcdfc0c
anton <antony99009@gmail.com>
2025-06-23 17:33:25 -0400

    tab and favcon

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,10 +1,19 @@
 import "@/styles/globals.css";
 import { SessionProvider } from "next-auth/react";
 import type { AppProps } from "next/app";
+import Head from "next/head";
 
 export default function App({ Component, pageProps: { session, ...pageProps } }: AppProps) {
   return (
     <SessionProvider session={session}>
+      <Head>
+        <title>Enemies List</title>
+        {/* Emoji Favicon (no file needed!) */}
+        <link 
+          rel="icon" 
+          href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>🎯</text></svg>" 
+        />
+      </Head>
       <Component {...pageProps} />
     </SessionProvider>
   )
